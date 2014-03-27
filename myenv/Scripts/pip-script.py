9a64ede74b933c8332bfe5aa05711003a50d0cfd
Dennis Zellich <dzelic@email.ht.hr>
2014-03-27 20:38:53 +0100

    initial

---
+++ myenv/Scripts/pip-script.py
@@ -0,0 +1,9 @@
+#!"c:\Documents and Settings\clasper\django_heroku_deploy\myenv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.4','console_scripts','pip'
+__requires__ = 'pip==1.4'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('pip==1.4', 'console_scripts', 'pip')()
+)
