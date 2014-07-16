7f564c938466e9587de818e402697e2ab64e000c
Dennis Zellich <dennis.zellich@gmail.com>
2014-07-16 11:06:44 +0200

    update project for heroku and S3

--- django15/settings.py
+++ django15/settings.py
@@ -161,6 +161,14 @@ try:
 except Exception as e:
   pass
 
+#Storage on S3 settings are stored as os.environs to keep settings.py clean
+if not DEBUG:
+   AWS_STORAGE_BUCKET_NAME = os.environ['django.heroku.deploy']
+   AWS_ACCESS_KEY_ID = os.environ['AKIAIOQ2KC4GZKCM2MTA']
+   AWS_SECRET_ACCESS_KEY = os.environ['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']
+   STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+   S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+   STATIC_URL = S3_URL  
 
 
 
