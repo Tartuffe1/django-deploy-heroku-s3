212ca931c283b1f72145e2a32a836ec72f330d21
Dennis Zellich <dennis.zellich@gmail.com>
2014-07-21 21:11:29 +0200

    latest7

--- django15/settings.py
+++ django15/settings.py
@@ -64,7 +64,6 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (os.path.join(SITE_ROOT, 'static'),)
 
 # List of finder classes that know how to find static files in
 # various locations.
