57544e23f2b5aaf06f1fa2c21806252ce990af02
Dennis Zellich <dennis.zellich@gmail.com>
2014-07-21 20:38:17 +0200

    latest4

--- django15/settings.py
+++ django15/settings.py
@@ -169,6 +169,7 @@ if not DEBUG:
    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
    STATIC_URL = S3_URL
+   
 
 
 
