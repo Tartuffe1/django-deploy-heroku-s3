863b62303712879012d3740b1abe62b3502e7726
Dennis Zellich <dennis.zellich@gmail.com>
2014-07-25 09:43:12 +0200

    amazon changes

--- django15/settings.py
+++ django15/settings.py
@@ -166,6 +166,8 @@ if not DEBUG:
    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+   # Use Amazon S3 for storage for uploaded media files.
+   DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
    STATIC_URL = S3_URL
 
